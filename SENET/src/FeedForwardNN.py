1d6fe040ec598976dfc5fa62f2076dc7b2b4b9e6
Jinfeng Lin <ljfnwpusife@gmail.com>
2018-04-22 17:39:59 -0400

    use batch

--- SENET/src/FeedForwardNN.py
+++ SENET/src/FeedForwardNN.py
@@ -16,10 +16,9 @@ from config import *
 class FNN:
     def __init__(self, vec_len, model_path, label_encoder_pickle):
         self.x_size = vec_len
-        self.h1_size = 128
-        self.h2_size = 64
+        self.h_size = 256
         self.y_size = 2
-        self.epoch = 200
+        self.epoch = 40
         self.batch = 100
         self.graph = tf.Graph()
         self.model_path = model_path
@@ -27,27 +26,17 @@ class FNN:
         with self.graph.as_default():
             self.X = tf.placeholder("float", shape=[None, self.x_size])
             self.y = tf.placeholder("float", shape=[None, self.y_size])
-
-            self.weights = {
-                'h1': tf.Variable(tf.random_normal([self.x_size, self.h1_size])),
-                'h2': tf.Variable(tf.random_normal([self.h1_size, self.h2_size])),
-                'out': tf.Variable(tf.random_normal([self.h2_size, self.y_size]))
-            }
-            self.biases = {
-                'b1': tf.Variable(tf.random_normal([self.h1_size])),
-                'b2': tf.Variable(tf.random_normal([self.h2_size])),
-                'out': tf.Variable(tf.random_normal([self.y_size]))
-            }
-
-            self.yhat = self.forwardprop(self.X)
+            w_1 = self.init_weights((self.x_size, self.h_size))
+            w_2 = self.init_weights((self.h_size, self.y_size))
+            self.yhat = self.forwardprop(self.X, w_1, w_2)
             self.predict = tf.argmax(self.yhat, axis=1)
             self.correct_pred = tf.equal(self.predict, tf.argmax(self.y, 1))
-            self.confidence = tf.nn.softmax(self.yhat)
+            self.confidence = tf.nn.softmax(self.forwardprop(self.X, w_1, w_2))
 
     def train(self, train_set: DataSet):
         with tf.Session(graph=self.graph) as sess:
             cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.yhat, labels=self.y))
-            updates = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+            updates = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
             init = tf.global_variables_initializer()
             saver = tf.train.Saver()
             sess.run(init)
@@ -56,7 +45,8 @@ class FNN:
                 # Train with each example
                 print("Running epoch {}".format(epoch))
                 for i in range(int(len(train_X) / self.batch)):
-                    sess.run(updates,feed_dict={self.X: train_X[i * self.batch: i + self.batch],self.y: train_y[i * self.batch: i + self.batch]})
+                    sess.run(updates, feed_dict={self.X: train_X[i * self.batch: (i + 1) * self.batch],
+                                                 self.y: train_y[i * self.batch: (i + 1) * self.batch]})
             saver.save(sess, self.model_path)
             with open(self.label_encoder_pickle, "wb") as encoder_fout:
                 pickle.dump(train_set.label_encoder, encoder_fout)
@@ -87,8 +77,8 @@ class FNN:
             saver = tf.train.Saver()
             saver.restore(sess, self.model_path)
             test_X, test_y, test_word_pair = test_set.all()
-            confidence_score = sess.run(self.confidence, feed_dict={self.X: test_X})
             is_correct = sess.run(self.correct_pred, feed_dict={self.X: test_X, self.y: test_y})
+            confidence_score = sess.run(self.confidence, feed_dict={self.X: test_X})
             res = (is_correct, test_word_pair, confidence_score)
         return res
 
@@ -148,21 +138,14 @@ class FNN:
         weights = tf.random_normal(shape, stddev=0.1)
         return tf.Variable(weights)
 
-    def forwardprop(self, X):
+    def forwardprop(self, X, w_1, w_2):
         """
         Forward-propagation.
         IMPORTANT: yhat is not softmax since TensorFlow's softmax_cross_entropy_with_logits() does that internally.
         """
-        # Hidden fully connected layer with 256 neurons
-        layer_1 = tf.add(tf.matmul(X, self.weights['h1']), self.biases['b1'])
-        # Hidden fully connected layer with 256 neurons
-        layer_2 = tf.add(tf.matmul(layer_1, self.weights['h2']), self.biases['b2'])
-        # Output fully connected layer with a neuron for each class
-        out_layer = tf.matmul(layer_2, self.weights['out']) + self.biases['out']
-        return out_layer
-        # h = tf.nn.sigmoid(tf.matmul(X, w_1))  # The \sigma function
-        # yhat = tf.matmul(h, w_2)  # The \varphi function
-        # return yhat
+        h = tf.nn.sigmoid(tf.matmul(X, w_1))  # The \sigma function
+        yhat = tf.matmul(h, w_2)  # The \varphi function
+        return yhat
 
     def ten_fold_test(self, data_set: DataPrepare):
         result_file = RESULT_DIR + os.sep + "FeedForward_Result{}.txt".format(len(os.listdir(RESULT_DIR)))
@@ -189,69 +172,73 @@ class FNN:
             print("Average Recall={}\nAverage Precision={}\nAverage F1={}\nAverage Accuracy={}\n".
                   format(a_recall / 10, a_pre / 10, a_f1 / 10, a_acc / 10))
 
-    # def train_test(self, data_set):
-    #     RANDOM_SEED = 42
-    #     tf.set_random_seed(RANDOM_SEED)
-    #     # Layer's sizes
-    #     x_size = data_set.get_vec_length()  # Length of the feature vector
-    #     h_size = self.h_size  # Number of hidden nodes
-    #     y_size = self.y_size  # classify as yes/no
-    #
-    #     # Symbols
-    #     X = tf.placeholder("float", shape=[None, x_size])
-    #     y = tf.placeholder("float", shape=[None, y_size])
-    #
-    #     # Weight initializations
-    #     w_1 = self.init_weights((x_size, h_size))
-    #     w_2 = self.init_weights((h_size, y_size))
-    #
-    #     # Forward propagation
-    #     yhat = self.forwardprop(X)
-    #     predict = tf.argmax(yhat, axis=1)
-    #     correct_pred = tf.equal(predict, tf.argmax(y, 1))
-    #
-    #     # Backward propagation
-    #     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=yhat))
-    #     updates = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
-    #
-    #     # Run SGD
-    #     with tf.Session() as sess:
-    #         result_file = RESULT_DIR + os.sep + "FeedForward_Result{}.txt".format(len(os.listdir(RESULT_DIR)))
-    #         result_csv = RESULT_DIR + os.sep + "csv" + os.sep + "FeedForward_result{}.csv".format(
-    #             len(os.listdir(RESULT_DIR)))
-    #         a_acc = a_recall = a_pre = a_f1 = 0
-    #         with open(result_file, "w", encoding='utf8') as fout, open(result_csv, "w", encoding='utf8') as csv_fout:
-    #             exp_data = data_set.ten_fold()
-    #             for index, (train_set, test_set) in enumerate(exp_data):
-    #                 train_X, train_y, train_word_pair = train_set.all()
-    #                 test_X, test_y, test_word_pair = test_set.all()
-    #                 self.confidence = tf.nn.softmax(self.forwardprop(X, w_1, w_2))
-    #                 print("Start fold {}".format(index))
-    #                 init = tf.global_variables_initializer()
-    #                 sess.run(init)
-    #                 res = []
-    #                 for epoch in range(self.epoch):
-    #                     # Train with each example
-    #                     print("Running epoch {}".format(epoch))
-    #                     for i in range(len(train_X)):
-    #                         sess.run(updates, feed_dict={X: train_X[i: i + 1], y: train_y[i: i + 1]})
-    #                 is_correct = sess.run(correct_pred, feed_dict={X: test_X, y: test_y})
-    #                 confidence_score = sess.run(self.confidence, feed_dict={X: test_X})
-    #                 res = (is_correct, test_word_pair, confidence_score)
-    #                 re, pre, f1, acc = self.eval(res, data.encoder)
-    #                 write_csv([re, pre, f1, acc], csv_fout)
-    #                 self.write_res(res, fout, data_set.encoder)
-    #                 a_recall += re
-    #                 a_pre += pre
-    #                 a_f1 += f1
-    #                 a_acc += acc
-    #
-    #             avg_str = "Average recall:{}, precision:{}, f1:{}, accuracy:{}".format(a_recall / 10, a_pre / 10,
-    #                                                                                    a_f1 / 10,
-    #                                                                                    a_acc / 10)
-    #             fout.write(avg_str)
-    #             print(avg_str)
-    #     tf.reset_default_graph()
+    def run(self):
+        data_set = DataPrepare()
+        self.train_test(data_set)
+
+    def train_test(self, data_set):
+        RANDOM_SEED = 42
+        tf.set_random_seed(RANDOM_SEED)
+        # Layer's sizes
+        x_size = data_set.get_vec_length()  # Length of the feature vector
+        h_size = self.h_size  # Number of hidden nodes
+        y_size = self.y_size  # classify as yes/no
+
+        # Symbols
+        X = tf.placeholder("float", shape=[None, x_size])
+        y = tf.placeholder("float", shape=[None, y_size])
+
+        # Weight initializations
+        w_1 = self.init_weights((x_size, h_size))
+        w_2 = self.init_weights((h_size, y_size))
+
+        # Forward propagation
+        yhat = self.forwardprop(X, w_1, w_2)
+        predict = tf.argmax(yhat, axis=1)
+        correct_pred = tf.equal(predict, tf.argmax(y, 1))
+
+        # Backward propagation
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=yhat))
+        updates = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+
+        # Run SGD
+        with tf.Session() as sess:
+            result_file = RESULT_DIR + os.sep + "FeedForward_Result{}.txt".format(len(os.listdir(RESULT_DIR)))
+            result_csv = RESULT_DIR + os.sep + "csv" + os.sep + "FeedForward_result{}.csv".format(
+                len(os.listdir(RESULT_DIR)))
+            a_acc = a_recall = a_pre = a_f1 = 0
+            with open(result_file, "w", encoding='utf8') as fout, open(result_csv, "w", encoding='utf8') as csv_fout:
+                exp_data = data_set.ten_fold()
+                for index, (train_set, test_set) in enumerate(exp_data):
+                    train_X, train_y, train_word_pair = train_set.all()
+                    test_X, test_y, test_word_pair = test_set.all()
+                    self.confidence = tf.nn.softmax(self.forwardprop(X, w_1, w_2))
+                    print("Start fold {}".format(index))
+                    init = tf.global_variables_initializer()
+                    sess.run(init)
+                    res = []
+                    for epoch in range(self.epoch):
+                        # Train with each example
+                        print("Running epoch {}".format(epoch))
+                        for i in range(len(train_X)):
+                            sess.run(updates, feed_dict={X: train_X[i: i + 1], y: train_y[i: i + 1]})
+                    is_correct = sess.run(correct_pred, feed_dict={X: test_X, y: test_y})
+                    confidence_score = sess.run(self.confidence, feed_dict={X: test_X})
+                    res = (is_correct, test_word_pair, confidence_score)
+                    re, pre, f1, acc = self.eval(res, data.encoder)
+                    write_csv([re, pre, f1, acc], csv_fout)
+                    self.write_res(res, fout, data_set.encoder)
+                    a_recall += re
+                    a_pre += pre
+                    a_f1 += f1
+                    a_acc += acc
+
+                avg_str = "Average recall:{}, precision:{}, f1:{}, accuracy:{}".format(a_recall / 10, a_pre / 10,
+                                                                                       a_f1 / 10,
+                                                                                       a_acc / 10)
+                fout.write(avg_str)
+                print(avg_str)
+        tf.reset_default_graph()
 
     def write_classify_res(self, file_path, res, label_encoder: Encoder):
         # TODO inlcude encoder information in result
@@ -268,5 +255,5 @@ if __name__ == '__main__':
         data = DataPrepare("dataset_origin.pickle", feature_pipe=None, raw_materials=None,
                            rebuild=False)
         fnn = FNN(data.get_vec_length(), FNN_MODEL_DIR, FNN_ENCODER_PATH)
-        fnn.ten_fold_test(data)
+        fnn.train_test(data)
         print("Round {} finished".format(i))
